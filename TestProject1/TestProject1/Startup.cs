524114b153f47ad5afe49127d36752001abbcabf
LXD24 <38374049+LXD24@users.noreply.github.com>
2024-08-17 15:18:01 +0800

    Update Startup.cs

--- TestProject1/TestProject1/Startup.cs
+++ TestProject1/TestProject1/Startup.cs
@@ -30,12 +30,12 @@ namespace TestProject1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Log.Logger = new LoggerConfiguration()
-            //    .ReadFrom.Configuration(Configuration)
-            //    .MinimumLevel.Warning()
-            //    .Enrich.FromLogContext()
-            //    .WriteTo.Console(new RenderedCompactJsonFormatter())
-            //    .CreateLogger();
+            Log.Logger = new LoggerConfiguration()
+               .ReadFrom.Configuration(Configuration)
+               .MinimumLevel.Warning()
+               .Enrich.FromLogContext()
+               .WriteTo.Console(new RenderedCompactJsonFormatter())
+               .CreateLogger();
 
             services.AddControllers(x =>
             {
